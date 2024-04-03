0011673bc7c09379677757645002cf0ac0aedbf5
Stephin Thomas <stephin_thomas@protonmail.com>
2024-04-04 00:50:18 +0100

    use GTK FilterListModel to filter table values

--- src/gui/table_win.rs
+++ src/gui/table_win.rs
@@ -1,9 +1,9 @@
+use adw::glib::clone;
 use gtk::{gio, glib::BoxedAnyObject, prelude::*, Orientation};
 use gtk::{Box, SingleSelection};
-
 use serde::de::DeserializeOwned;
 
-use std::cell::Ref;
+use std::cell::{Ref, RefCell};
 use std::path::Path;
 
 use crate::gui::gobjects;
@@ -42,10 +42,8 @@ pub(crate) fn create<S, T>(
         .icon_name(APP_NAME)
         .modal(true)
         .build();
-    // window.set_transient_for(Some(parent_win.as_ref()));
-    // let window = Rc::new(window);
-    let roles_win = draw(csv_data, header, filter_index, filter_dropdown, col_width);
-    window.set_content(Some(&roles_win));
+    let table_box = draw(csv_data, header, filter_index, filter_dropdown, col_width);
+    window.set_content(Some(&table_box));
 
     let action_close = ActionEntry::builder("close")
         .activate(|window: &ApplicationWindow, _, _| {
@@ -64,7 +62,7 @@ pub(crate) fn draw<T>(
     csv_data: Rc<Vec<T>>,
     header: &[&'static str],
     filter_index: usize,
-    filter_dropdown: Option<Vec<String>>,
+    filter_vals: Option<Vec<String>>,
     col_width: Option<&[i32]>,
 ) -> Box
 where
@@ -72,11 +70,28 @@ where
     T: Clone,
     T: 'static,
 {
+    let val_dropdown = Rc::new(RefCell::new("None".to_owned()));
     // Create columns and add them to the table
-    let store = filter_list(Rc::clone(&csv_data), "None".to_owned(), filter_index);
+    let list_store = Rc::new(gen_list_store(Rc::clone(&csv_data)));
+    let cust_filter = gtk::CustomFilter::new(clone!(@strong val_dropdown=> move |obj| {
+        let record = obj.downcast_ref::<BoxedAnyObject>().unwrap();
+        let r: Ref<T> = record.borrow();
+        let val_dropdown_ref=val_dropdown.borrow();
+        if r.as_array()[filter_index] == *val_dropdown_ref|| *val_dropdown_ref == "None".to_owned()  {
+        println!("{}=={}\n",val_dropdown_ref, r.as_array()[filter_index]);
+            return true;
+        } else {
+        println!("{}!={}\n",val_dropdown_ref, r.as_array()[filter_index]);
+            return false;
+        }
+    }));
+    let filter_store = gtk::FilterListModel::builder()
+        .model(list_store.as_ref())
+        .filter(&cust_filter.clone())
+        .build();
+    let sel = gtk::SingleSelection::builder().model(&filter_store).build();
 
-    let sel = gtk::SingleSelection::new(Some(store));
-    let columnview = Rc::new(gtk::ColumnView::new(Some(sel)));
+    let columnview = gtk::ColumnView::new(Some(sel));
     if let Some(col_w) = col_width {
         if col_w.len() != header.len() {
             panic!("Header and Column widths don't match")
@@ -108,12 +123,11 @@ where
         };
         columnview.append_column(&col)
     }
-    let column_view_rc = Rc::clone(&columnview);
     let scrolled_window = gtk::ScrolledWindow::builder()
         // .hscrollbar_policy(gtk::PolicyType::Never) // Disable horizontal scrolling
         .build();
 
-    scrolled_window.set_child(Some(columnview.as_ref()));
+    scrolled_window.set_child(Some(&columnview));
     scrolled_window.set_vexpand(true);
     scrolled_window.set_min_content_height(250);
     let vbox = Box::builder()
@@ -123,53 +137,66 @@ where
     vbox.append(&adw::HeaderBar::new());
     vbox.append(&scrolled_window);
 
-    if let Some(filter_dropdown) = filter_dropdown {
-        let drop_down_str: Vec<&str> = filter_dropdown.iter().map(String::as_ref).collect();
-        let role_dropdown = gtk::DropDown::from_strings(drop_down_str.as_slice());
-        role_dropdown.set_size_request(200, 1);
-        role_dropdown.set_halign(gtk::Align::End);
-        let filter_dropdown_1 = filter_dropdown.clone();
-        role_dropdown.connect_selected_notify(move |signal| {
-            let sign = signal.selected();
-            let role = filter_dropdown_1
-                .iter()
-                .enumerate()
-                .find(
-                    move |(index, _)| {
-                        if *index == sign as usize {
-                            true
-                        } else {
-                            false
-                        }
-                    },
-                )
-                .unwrap();
-            let model = column_view_rc.model().unwrap();
+    let filter_dropdown: Option<gtk::DropDown> = match filter_vals {
+        Some(filter_dropdown_vals) => {
+            let drop_down_str: Vec<&str> =
+                filter_dropdown_vals.iter().map(String::as_ref).collect();
+            let drop_down = gtk::DropDown::from_strings(drop_down_str.as_slice());
+            drop_down.set_size_request(200, 1);
+            drop_down.set_halign(gtk::Align::End);
+            drop_down.connect_selected_notify(clone!(@strong val_dropdown=> move |signal| {
+                let sign = signal.selected();
+                let val_dropdown_mut = filter_dropdown_vals
+                    .iter()
+                    .enumerate()
+                    .find(
+                        move |(index, _)| {
+                            if *index == sign as usize {
+                                true
+                            } else {
+                                false
+                            }
+                        },
+                    )
+                    .unwrap()
+                    .1
+                    .to_owned();
+                val_dropdown.replace(val_dropdown_mut);
+            let model = columnview.model().unwrap();
             let single_select_model: SingleSelection = model.downcast().unwrap();
-            let store = filter_list(Rc::clone(&csv_data), (*role.1).to_owned(), filter_index);
-            single_select_model.set_model(Some(&store));
-            column_view_rc.set_model(Some(&single_select_model));
-        });
+            let filter_store = gtk::FilterListModel::builder()
+                .model(list_store.as_ref())
+                .filter(&cust_filter.clone())
+                .build();
+            single_select_model.set_model(Some(&filter_store));
+            }));
 
-        let hbox = Box::builder()
-            .orientation(Orientation::Horizontal)
-            .homogeneous(true)
-            .build();
-        let filter_lbl = gobjects::create_generic_label(gtk::Justification::Left);
-        filter_lbl.set_label("Filter");
-        hbox.append(&filter_lbl);
-        hbox.append(&role_dropdown);
-        vbox.append(&hbox);
+            Some(drop_down)
+        }
+        None => {
+            println!("No DropDown");
+            None
+        }
     };
+
+    let hbox = Box::builder()
+        .orientation(Orientation::Horizontal)
+        .homogeneous(true)
+        .build();
+    let filter_lbl = gobjects::create_generic_label(gtk::Justification::Left);
+    filter_lbl.set_label("Filter");
+    hbox.append(&filter_lbl);
+    if filter_dropdown.is_some() {
+        hbox.append(filter_dropdown.as_ref().unwrap());
+    }
+    vbox.append(&hbox);
+
     vbox.set_css_classes(&["boxed-list"]);
+
     return vbox;
 }
 
-fn filter_list<T>(
-    role_csv_data: Rc<Vec<T>>,
-    filter_str: String,
-    filter_index: usize,
-) -> gio::ListStore
+fn gen_list_store<T>(role_csv_data: Rc<Vec<T>>) -> gio::ListStore
 where
     T: AsArray,
     T: Clone,
@@ -177,10 +204,7 @@ where
 {
     let store = gio::ListStore::new::<BoxedAnyObject>();
     for record in role_csv_data.as_ref() {
-        if record.as_array()[filter_index] == filter_str || "None" == filter_str {
-            store.append(&BoxedAnyObject::new(record.clone()))
-        }
+        store.append(&BoxedAnyObject::new(record.clone()))
     }
     store
 }
-//close this window when the main window closes
